6a6c1acc2d09171d12ce77f6c61732a07272c7b2
michael-alan-mcauliffe <michael.alan.mcauliffe@gmail.com>
2021-03-30 11:20:48 -0400

    Added main body and navbar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainBodyComponent } from './main-body/main-body.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainBodyComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
