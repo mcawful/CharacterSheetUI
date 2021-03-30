aff3dbc3a9b2e9e333e997d287d9f268eae47075
michael-alan-mcauliffe <michael.alan.mcauliffe@gmail.com>
2021-03-30 11:35:15 -0400

    Fixed imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainBodyComponent } from './main-body/main-body.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { MainBodyComponent } from './components/main-body/main-body.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { StoreModule } from '@ngrx/store';
 
 @NgModule({
