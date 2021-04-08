12dab4bc54b3608e9ffd29ae57750c852700dba0
michael-alan-mcauliffe <michael.alan.mcauliffe@gmail.com>
2021-04-08 10:43:44 -0400

    Fixed dependency names

--- src/app/models/player.model.ts
+++ src/app/models/player.model.ts
@@ -1,13 +1,13 @@
-import { ActionSkills } from './action-skills-model';
-import { Cosmetics } from './cosmetics-model';
-import { SavingThrows } from './saving-throws-model';
+import { ActionSkills } from './action-skills.model';
+import { Cosmetics } from './cosmetics.model';
+import { SavingThrows } from './saving-throws.model';
 
 export interface Player {
     playerName: string,
     characterName: string,
     alignment: string,
     size: string,
-    
+
     cosmetics: Cosmetics,
 
     hitPoints: number,
