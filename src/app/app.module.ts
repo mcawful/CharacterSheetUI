7ae2dd0c3d04868388f8a5d7eb230d59bbf5bbf5
michael-alan-mcauliffe <michael.alan.mcauliffe@gmail.com>
2021-04-08 10:36:45 -0400

    Revert "Proper install of NgRx"
    This reverts commit 4bb8da49fbb6549a2e80e8ecfbfa7e699ed9b894.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,6 @@ import { AppComponent } from './app.component';
 import { MainBodyViewComponent } from './components/main-body-view/main-body-view.component';
 import { NavbarViewComponent } from './components/navbar-view/navbar-view.component';
 import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { EffectsModule } from '@ngrx/effects';
-import { AppEffects } from './app.effects';
 
 @NgModule({
   declarations: [
@@ -21,10 +16,7 @@ import { AppEffects } from './app.effects';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {}),
-    StoreModule.forRoot(reducers, { metaReducers }),
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
-    EffectsModule.forRoot([AppEffects])
+    StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.state.ts
+++
@@ -1,5 +0,0 @@
-import { Player } from './models/player.model';
-
-export interface AppState {
-    readonly player: Player[];
-}
