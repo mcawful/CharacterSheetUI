7c0eba20fce515e075b11457d6ea3998ff04fa8e
michael-alan-mcauliffe <michael.alan.mcauliffe@gmail.com>
2021-04-08 10:36:32 -0400

    Revert "Created Player actions and reducer"
    This reverts commit 42d8bd168163d8145315e0b57a0eb8ca5c11fa05.

--- src/app/actions/player.actions.spec.ts
+++
@@ -1,7 +0,0 @@
-import * as fromPlayer from './player.actions';
-
-describe('loadPlayers', () => {
-  it('should return an action', () => {
-    expect(fromPlayer.loadPlayers().type).toBe('[Player] Load Players');
-  });
-});
